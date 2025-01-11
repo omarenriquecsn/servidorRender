e094a7030cd008492e1a55ba16790ca8ed902bfe
unknown <omarenriquecs@gmail.com>
2025-01-11 09:34:48 -0400

    hola

--- express/server.js
+++ express/server.js
@@ -1,3 +1,192 @@
+// "use strict";
+// const express = require("express");
+// const path = require("path");
+// const serverless = require("serverless-http");
+// const app = express();
+// const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const cors = require("cors");
+
+
+// app.listen(3000, () => console.log('Local app listening on port 3000!'));
+
+// mongoose.connect(
+//   "mongodb+srv://omarenriquecs:P2wEdgjBpWWBxUtS@prueba.1w2j9.mongodb.net/Sumichen"
+// );
+
+// const Schema = mongoose.Schema;
+
+// const schemaGlobalca = new Schema({
+//   id: Number,
+//   producto: String,
+//   fechaIn: String,
+//   fechaVen: String,
+//   lote: String,
+//   cantidadDisponible: Number,
+//   cantidadApartada: Number,
+//   almacen: String,
+// });
+
+// const Globalca = mongoose.model("Globalca", schemaGlobalca);
+
+// const schemaWMS = new Schema({
+//   id: Number,
+//   producto: String,
+//   fechaIn: String,
+//   fechaVen: String,
+//   lote: String,
+//   cantidadDisponible: Number,
+//   cantidadApartada: Number,
+//   almacen: String,
+// });
+
+// const WMS = mongoose.model("WMS", schemaWMS);
+
+// const schemaDespachos = new Schema({
+//   id: Number,
+//   name: String,
+//   producto: String,
+//   cantidad: Number,
+//   date: String,
+//   status: String,
+// });
+
+// const Despachos = mongoose.model("Despachos", schemaDespachos);
+
+// const schemaClientes = new Schema({
+//   id: Number,
+//   name: String,
+//   rif: Number,
+// });
+
+// const Clientes = mongoose.model("Clientes", schemaClientes);
+
+// const importGlobalca = async () => {
+//   const lista = await Globalca.find();
+//   return lista;
+// };
+
+// const importWMS = async () => {
+//   const lista = await WMS.find();
+//   return lista;
+// };
+
+// const importDespachos = async () => {
+//   const lista = await Despachos.find();
+//   return lista;
+// };
+
+// const importClientes = async () => {
+//   const lista = await Clientes.find();
+//   return lista;
+// };
+// const router = express.Router();
+
+// router.put("/", (req, res) => {
+//   console.log(req.body);
+// });
+
+// router.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../Globalca.html"));
+//   res.end();
+// });
+
+// router.post("/", (req, res) =>
+//   res.sendFile(path.join(__dirname, "../Globalca.html"))
+// );
+
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use("/.netlify/functions/server", router); // path must route to lambda
+
+// app.use("/Globalca", async (req, res) => {
+//   if (req.body.producto) {
+//     try {
+//       await Globalca.create(req.body);
+//       res.json(req.body);
+     
+//     } catch(error) {
+//       console.log(error);
+//     }
+//   } else if (req.body.change) {
+//     const filter = { id: req.body.id };
+//     const update = {
+//       $set: {
+//         cantidadDisponible: req.body.cantidadDisponible,
+//         cantidadApartada: req.body.cantidadApartada,
+//       },
+//     };
+//     const opts = { new: true };
+//     console.log(req.body);
+//     const objectUpdate = await Globalca.findOneAndUpdate(filter, update, opts);
+//   } else {
+//     const productos = await importGlobalca();
+   
+//     res.json(productos);
+//   }
+// });
+
+// app.use("/WMS", async (req, res) => {
+//   if (req.body.producto) {
+//     WMS.create(req.body);
+//     res.json(req.body);
+   
+//   } else if (req.body.change) {
+//     const filter = { id: req.body.id };
+//     const update = {
+//       $set: {
+//         cantidadDisponible: req.body.cantidadDisponible,
+//         cantidadApartada: req.body.cantidadApartada,
+//       },
+//     };
+//     const opts = { new: true };
+//     const objectUpdate = await WMS.findOneAndUpdate(filter, update, opts);
+//   } else {
+//     const productos = await importWMS();
+    
+//     res.json(productos);
+//   }
+// });
+
+// app.use("/Despachos", async (req, res) => {
+//   if (req.body.producto) {
+//     Despachos.create(req.body);
+//     res.json(req.body);
+    
+//   }
+//     else if(req.body.change){
+//       const filter = { id: req.body.id };
+//     const update = {
+//       $set: {
+//         status: req.body.status
+//       },
+//     };
+//     const opts = { new: true };
+//     const objectUpdate = await WMS.findOneAndUpdate(filter, update, opts);
+//     }
+//    else {
+//     const productos = await importDespachos();
+//     res.json(productos);
+    
+//   }
+// });
+
+// app.use("/Clientes", async (req, res) => {
+//   if (req.body.name) {
+//     Clientes.create(req.body);
+//     res.json(req.body);
+//     console.log("post");
+//   } else {
+//     const productos = await importClientes();
+//     console.log("clientes");
+//     res.json(productos);
+//   }
+// });
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
+
+
 "use strict";
 const express = require("express");
 const path = require("path");
@@ -6,6 +195,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
+app.listen(process.env.PORT || 3000, () => console.log('Local app listening on port 3000!'));
 
 mongoose.connect(
   "mongodb+srv://omarenriquecs:P2wEdgjBpWWBxUtS@prueba.1w2j9.mongodb.net/Sumichen"
@@ -77,6 +268,7 @@ const importClientes = async () => {
   const lista = await Clientes.find();
   return lista;
 };
+
 const router = express.Router();
 
 router.put("/", (req, res) => {
@@ -101,8 +293,7 @@ app.use("/Globalca", async (req, res) => {
     try {
       await Globalca.create(req.body);
       res.json(req.body);
-     
-    } catch(error) {
+    } catch (error) {
       console.log(error);
     }
   } else if (req.body.change) {
@@ -118,16 +309,18 @@ app.use("/Globalca", async (req, res) => {
     const objectUpdate = await Globalca.findOneAndUpdate(filter, update, opts);
   } else {
     const productos = await importGlobalca();
-   
     res.json(productos);
   }
 });
 
 app.use("/WMS", async (req, res) => {
   if (req.body.producto) {
-    WMS.create(req.body);
-    res.json(req.body);
-   
+    try {
+      await WMS.create(req.body);
+      res.json(req.body);
+    } catch (error) {
+      console.log(error);
+    }
   } else if (req.body.change) {
     const filter = { id: req.body.id };
     const update = {
@@ -140,39 +333,42 @@ app.use("/WMS", async (req, res) => {
     const objectUpdate = await WMS.findOneAndUpdate(filter, update, opts);
   } else {
     const productos = await importWMS();
-    
     res.json(productos);
   }
 });
 
 app.use("/Despachos", async (req, res) => {
   if (req.body.producto) {
-    Despachos.create(req.body);
-    res.json(req.body);
-    
-  }
-    else if(req.body.change){
-      const filter = { id: req.body.id };
+    try {
+      await Despachos.create(req.body);
+      res.json(req.body);
+    } catch (error) {
+      console.log(error);
+    }
+  } else if (req.body.change) {
+    const filter = { id: req.body.id };
     const update = {
       $set: {
         status: req.body.status
       },
     };
     const opts = { new: true };
-    const objectUpdate = await WMS.findOneAndUpdate(filter, update, opts);
-    }
-   else {
+    const objectUpdate = await Despachos.findOneAndUpdate(filter, update, opts);
+  } else {
     const productos = await importDespachos();
     res.json(productos);
-    
   }
 });
 
 app.use("/Clientes", async (req, res) => {
   if (req.body.name) {
-    Clientes.create(req.body);
-    res.json(req.body);
-    console.log("post");
+    try {
+      await Clientes.create(req.body);
+      res.json(req.body);
+      console.log("post");
+    } catch (error) {
+      console.log(error);
+    }
   } else {
     const productos = await importClientes();
     console.log("clientes");
